b43722de06293d879f05108d0ad34f2bd66a6145
Pavel Procházka <pprochazka72@gmail.com>
2016-10-20 19:49:26 +0200

    Add some content

--- PiedPiper/AppDelegate.swift
+++ PiedPiper/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
+//		
+//		UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+//		UINavigationBar.appearance().backgroundColor = UIColor(colorLiteralRed: 46, green: 157, blue: 72, alpha: 1.0)
+		
 		return true
 	}
 
